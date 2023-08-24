b1a3d4a0c9fc7d9a14648abe5e9910b05e33c65d
David Gikubu <davidndungu8203@gmail.com>
2023-08-24 22:45:12 +0300

    this is a fully developed pizza bill calculator for you enjoy.

--- src/JavaPizza.java
+++ src/JavaPizza.java
@@ -10,12 +10,51 @@ import java.util.*;
 public class JavaPizza {
     public static void main(String[] args) {
     Scanner reader = new Scanner(System.in);
-        System.out.println("welcome to java pizza.");
-        System.out.println("Size: 'Y' for 'yes' and 'N' for 'no'. ");
-        String size  = reader.nextLine();
-        System.out.println("Add Peperoni? 'Y' for 'yes' and 'N' for 'no'. ");
-        String peperoni = reader.nextLine();
-        System.out.println("Add extra cheese? 'Y' for 'yes' and 'N' for 'no'. ");
-        String extraCheese = reader.nextLine();
+    
+    int smallPizza = 1500;
+    int mediumPizza = 2000;
+    int largePizza = 2500;
+    int peperoniSmall = 200;
+    int peperoniMediumAndLarge = 300;
+    int extraCheeseForAllSizes = 100;
+
+    int bill;
+    
+    System.out.println("welcome to java pizza.");
+    System.out.println("What Pizza size do you want? Small(S),Medium(M) or Large(L)?");
+    String size  = reader.nextLine().toLowerCase();
+    System.out.println("Add Peperoni? 'Y' for 'yes' and 'N' for 'no'. ");
+    String peperoni = reader.nextLine().toLowerCase();
+    System.out.println("Add extra cheese? 'Y' for 'yes' and 'N' for 'no'. ");
+    String extraCheese = reader.nextLine().toLowerCase();
+    
+    if (size.equals("s") && peperoni.equals("y") && extraCheese.equals("y")) {
+        bill = smallPizza + peperoniSmall + extraCheeseForAllSizes;
+        System.out.println("your bill is " + bill);
+    } else if (size.equals("s") && peperoni.equals("n") && extraCheese.equals("y")) {
+        bill = smallPizza + extraCheeseForAllSizes;
+        System.out.println("your bill is " + bill);
+    } else if (size.equals("s") && peperoni.equals("n") && extraCheese.equals("n")) {
+        bill = smallPizza;
+        System.out.println("your bill is " + bill);
+    } else if (size.equals("m") && peperoni.equals("y") && extraCheese.equals("y")) {
+        bill = mediumPizza + peperoniMediumAndLarge + extraCheeseForAllSizes;
+        System.out.println("your bill is " + bill);
+    } else if (size.equals("m") && peperoni.equals("n") && extraCheese.equals("y")) {
+        bill = mediumPizza + extraCheeseForAllSizes;
+        System.out.println("your bill is " + bill);
+    } else if (size.equals("m") && peperoni.equals("n") && extraCheese.equals("n")) {
+        bill = mediumPizza;
+        System.out.println("your bill is " + bill);
+    } else if (size.equals("l") && peperoni.equals("y") && extraCheese.equals("y")) {
+        bill = largePizza + peperoniMediumAndLarge  + extraCheeseForAllSizes;
+        System.out.println("your bill is " + bill);
+    } else if (size.equals("l") && peperoni.equals("n") && extraCheese.equals("y")) {
+        bill = largePizza + extraCheeseForAllSizes;
+        System.out.println("your bill is " + bill);
+    }else if (size.equals("l") && peperoni.equals("n") && extraCheese.equals("n")) {
+        bill = largePizza;
+        System.out.println("your bill is " + bill);
+        }
     }
 }
